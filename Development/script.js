c9f28c46e6994d1a9e86d8ed6d07cbf4f7a5f4c9
Aaron Dreyer <aaronottaway@gmail.com>
2023-02-09 18:34:15 -0500

    Current Styling
    Added current styling from teammates to add my new javascript

--- Development/script.js
+++ Development/script.js
@@ -2,11 +2,6 @@
 
 let currentMemeIndex = 0;
 
-// Search History Array
-let savedHistory = [];
-
-let quoteData = [];
-
 fetch('https://api.imgflip.com/get_memes')
 	.then(response => response.json())
 	.then(response => {
@@ -22,8 +17,8 @@ fetch('https://api.imgflip.com/get_memes')
 				displayMeme(memes, currentMemeIndex);
 			} else if (event.key === "ArrowRight") {
 				currentMemeIndex++;
-				if (currentMemeIndex >= memes.length) {
-					currentMemeIndex = 0;
+					if (currentMemeIndex >= memes.length) {
+				 	currentMemeIndex = 0;
 				}
 				displayMeme(memes, currentMemeIndex);
 			}
@@ -36,83 +31,44 @@ function displayMeme(memes, index) {
 	memeContainer.innerHTML = `<img src="${memes[index].url}" alt="Meme">`;
 }
 
+// Advice API ------------------------------------------------------------- //
+// Add event listener on button
 $("#generate-text").on("click", setQuote);
 // Calls this future to reset or call text under image
 function setQuote() {
 fetch('https://api.adviceslip.com/advice')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        const quote = data.slip.advice;
-        console.log(quote);
-        $("#quote-box-text").text(quote);
-    })
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(data) {
+		var quote = data.slip.advice;
+		console.log(quote);
+
+		$("#quote-box-text").text(quote);
+	})
 };
+// MODAL  ----------------------------------------------------------------- //
+// var modal = document.querySelector('.modal');
+// var closeButtons = document.querySelectorAll('.close-modal');
+// // set open modal behaviour
 
-
-// Local Storage Code
-// Still deciding on path to take
-
-// // Search History Function for Memes
-
-// // Function to save memes to local storage
-// let saveHistoryData = function (memes) {
-
-// 	// if statement to display saved item in save history list unless if it has already been saved
-// 	if(!savedHistory.includes(memes)){
-// 		savedHistory.push(memes);
-// 		$("#search-history").append("<a href='#' id='" + memes + "'>" + memes + "</a>")
-// 	}
-
-// 	// Saves saved history data to local storage
-// 	localStorage.setItem("savedHistoryData", JSON.stringify(savedHistory));
-
-// 	// Saves quote data to storage
-// 	localStorage.setItem("quote", JSON.stringify(quote));
-
-// 	// Displays previously saved data
-// 	loadHistoryData();
-// };
-
-// // FUnction to display to load saved data
-// let loadHistoryData = function () {
-
-// 	// Retrieves saved data from local storage
-// 	savedHistory = JSON.parse(localStorage.getItem("savedHistoryData"));
-
-// 	// Retireves te quote data from local storage
-// 	quote = JSON.parse(localStorage.getItem("quote"));
-
-// 	// if no data is present then creates empty array
-// 	if (!savedHistory) {
-//         savedHistory = []
-//     }
-
-//     if (!quoteData) {
-//         quoteData = []
-//     }
-
-// 	// Clears any previous data from the saved history unordered list
-// 	$("#search-history").empty();
-
-// 	 // Converts the displayed meme in saved history to link to pull data from storage if clicked
-//     // Append to unordered list
-// 	for(i = 0 ; i < savedHistory.length ;i++) {
-
-//         $("#search-history").append("<a href='#' id='" + savedHistory[i] + "'>" + savedHistory[i] + "</a>");
-//     }
-// };
-
-
-// $("#search-form").submit(formSubmit);
-
-// // Event Handlers
-// $("#search-history").on("click", function(event){
-
-// 	// Retrieves links value from id
-//     let pastMeme = $(event.target).closest("a").attr("id");
-
-// 	// Pushes id's link value to displayMeme function
-//     displayMeme(pastMeme);
+// // set close modal behaviour
+// for (i = 0; i < closeButtons.length; ++i) {
+//   closeButtons[i].addEventListener('click', function() {
+//     modal.classList.toggle('modal-open');
+// 	});
+// }
+// // close modal if clicked outside content area
+// document.querySelector('.modal-inner').addEventListener('click', function() {
+//   modal.classList.toggle('modal-open');
 // });
+// // prevent modal inner from closing parent when clicked
+// document.querySelector('.modal-content').addEventListener('click', function(e) {
+// 	e.stopPropagation();
+// });
+// // Allows escape and space key to close for accessiblity purposes. 
+// document.addEventListener("keydown", function(event) {
+// 	if (event.key === "Escape" || event.key === " ") {
+// 		modal.classList.toggle('modal-open');
+// 	}
+// }); 
