5e511013ea727487331306bab4ec378f9d191bcd
Timothy Lai <timothylai1121@gmail.com>
2023-02-08 19:49:00 -0500

    classwork together

---
+++ Development/script.js
@@ -0,0 +1,32 @@
+// Api Imgflip links: 'https://api.imgflip.com/get_memes' //
+
+let currentMemeIndex = 0;
+
+fetch('https://api.imgflip.com/get_memes')
+	.then(response => response.json())
+	.then(response => {
+		const memes = response.data.memes;
+		displayMeme(memes, currentMemeIndex);
+
+		document.addEventListener("keydown", function(event) {
+			if (event.key === "ArrowLeft") {
+				currentMemeIndex--;
+				if (currentMemeIndex < 0) {
+					currentMemeIndex = memes.length - 1;
+				}
+				displayMeme(memes, currentMemeIndex);
+			} else if (event.key === "ArrowRight") {
+				currentMemeIndex++;
+				if (currentMemeIndex >= memes.length) {
+					currentMemeIndex = 0;
+				}
+				displayMeme(memes, currentMemeIndex);
+			}
+		});
+	})
+	.catch(err => console.error(err));
+
+function displayMeme(memes, index) {
+	const memeContainer = document.getElementById("memeContainer");
+	memeContainer.innerHTML = `<img src="${memes[index].url}" alt="Meme">`;
+}
