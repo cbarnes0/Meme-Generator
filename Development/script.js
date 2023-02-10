37d9e0879fced83a2c540b18ac85833185cb7f7b
Aaron Dreyer <aaronottaway@gmail.com>
2023-02-10 14:11:21 -0500

    Saving buttons to list
    I am attempting to save both the current image and quote to a single button into a side list to be clicked and return the saved image and text from local storage.

--- Development/script.js
+++ Development/script.js
@@ -1,7 +1,6 @@
 // Api Imgflip links: 'https://api.imgflip.com/get_memes' //
 
 let currentMemeIndex = 0;
-
 fetch('https://api.imgflip.com/get_memes')
 	.then(response => response.json())
 	.then(response => {
@@ -26,13 +25,12 @@ fetch('https://api.imgflip.com/get_memes')
 	})
 	.catch(err => console.error(err));
 
-function displayMeme(memes, index) {
-	const memeContainer = document.getElementById("memeContainer");
-	memeContainer.innerHTML = `<img src="${memes[index].url}" id="current-meme" alt="Meme">`;
-}
+
 
 // Advice API ------------------------------------------------------------- //
 // Add event listener on button
+var quote;
+
 $("#generate-text").on("click", setQuote);
 // Calls this future to reset or call text under image
 function setQuote() {
@@ -41,55 +39,60 @@ fetch('https://api.adviceslip.com/advice')
 		return response.json();
 	})
 	.then(function(data) {
-		var quote = data.slip.advice;
-		console.log(quote);
+		const quotes = data.slip.advice;
 
-		$("#quote-box-text").text(quote);
+		$("#quote-box-text").text(quotes);
 	})
 };
 
-// MODAL  ----------------------------------------------------------------- //
-// var modal = document.querySelector('.modal');
-// var closeButtons = document.querySelectorAll('.close-modal');
-// // set open modal behaviour
-
-// // set close modal behaviour
-// for (i = 0; i < closeButtons.length; ++i) {
-//   closeButtons[i].addEventListener('click', function() {
-//     modal.classList.toggle('modal-open');
-// 	});
-// }
-// // close modal if clicked outside content area
-// document.querySelector('.modal-inner').addEventListener('click', function() {
-//   modal.classList.toggle('modal-open');
-// });
-// // prevent modal inner from closing parent when clicked
-// document.querySelector('.modal-content').addEventListener('click', function(e) {
-// 	e.stopPropagation();
-// });
-// // Allows escape and space key to close for accessiblity purposes. 
-// document.addEventListener("keydown", function(event) {
-// 	if (event.key === "Escape" || event.key === " ") {
-// 		modal.classList.toggle('modal-open');
-// 	}
-// }); 
-
+  function displayMeme(memes, index) {
+	const memeContainer = document.getElementById("memeContainer");
+	memeContainer.innerHTML = `<img src="${memes[index].url}" id="current-meme" alt="Meme">`;
+}
 
 $(document).ready(function() {
-	$("#save-data").click(function() {
-	  var quoteText = $("#quote-box-text").text();
-	  var memeImage = $("#current-meme").attr("src");
-	  console.log(memeImage);
-	  localStorage.setItem("quoteText", quoteText);
-	  localStorage.setItem("memeImage", memeImage);
-	});
+    $("#save-data").click(function() {
+      var quoteText = $("#quote-box-text").text();
+      var memeImage = $("#current-meme").attr("src");
+      console.log(memeImage);
+      localStorage.setItem("quoteText", quoteText);
+      localStorage.setItem("memeImage", memeImage);
+    });
+    if (localStorage.getItem("quoteText")) {
+      $("#quote-box-text").text(localStorage.getItem("quoteText"));
+    }
+    if (localStorage.getItem("memeImage")) {
+      $("#current-meme").attr("src", localStorage.getItem("memeImage"));
+    }
+  });
 
-	if (localStorage.getItem("quoteText")) {
-	  $("#quote-box-text").text(localStorage.getItem("quoteText"));
-	}
-	if (localStorage.getItem("memeImage")) {
-	  $("#current-meme").attr("src", localStorage.getItem("memeImage"));
+  $(document).ready(function() {
+	var keys = Object.keys(localStorage);
+	for (var i = 0; i < keys.length; i++) {
+	  if (keys[i].startsWith("memeImage") || keys[i].startsWith("quoteText")) {
+		(function(i) {
+		  var memeImage = localStorage.getItem("memeImage" + i);
+		  var quoteText = localStorage.getItem("quoteText" + i);
+		  $("#button-container").append(`<button class="meme-button">Meme ${i + 1}</button>`);
+		  $(".meme-button").last().click(function() {
+			$("#quote-box-text").text(quoteText);
+			$("#current-meme").attr("src", memeImage);
+		  });
+		})(i);
+	  }
 	}
   });
+
+  
+
+
+
+  
+  
+
+  
+  
+  
+  
   
   
