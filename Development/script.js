88728b788cf5c7c17c9b3167ec251c64ce09359a
Aaron Dreyer <aaronottaway@gmail.com>
2023-02-10 12:06:46 -0500

    Merge
    I've added the code updates from my branch Saving to Local Storage to resolve the conflicts between each branch to merge successfully.

--- Development/script.js
+++ Development/script.js
@@ -3,32 +3,32 @@
 let currentMemeIndex = 0;
 
 fetch('https://api.imgflip.com/get_memes')
-	.then(response => response.json())
-	.then(response => {
-		const memes = response.data.memes;
-		displayMeme(memes, currentMemeIndex);
+    .then(response => response.json())
+    .then(response => {
+        const memes = response.data.memes;
+        displayMeme(memes, currentMemeIndex);
         // Adding eventlistener for arrow key left and right //
-		document.addEventListener("keydown", function(event) {
-			if (event.key === "ArrowLeft") {
-				currentMemeIndex--;
-				if (currentMemeIndex < 0) {
-					currentMemeIndex = memes.length - 1;
-				}
-				displayMeme(memes, currentMemeIndex);
-			} else if (event.key === "ArrowRight") {
-				currentMemeIndex++;
-					if (currentMemeIndex >= memes.length) {
-				 	currentMemeIndex = 0;
-				}
-				displayMeme(memes, currentMemeIndex);
-			}
-		});
-	})
-	.catch(err => console.error(err));
+        document.addEventListener("keydown", function(event) {
+            if (event.key === "ArrowLeft") {
+                currentMemeIndex--;
+                if (currentMemeIndex < 0) {
+                    currentMemeIndex = memes.length - 1;
+                }
+                displayMeme(memes, currentMemeIndex);
+            } else if (event.key === "ArrowRight") {
+                currentMemeIndex++;
+                    if (currentMemeIndex >= memes.length) {
+                    currentMemeIndex = 0;
+                }
+                displayMeme(memes, currentMemeIndex);
+            }
+        });
+    })
+    .catch(err => console.error(err));
 
 function displayMeme(memes, index) {
-	const memeContainer = document.getElementById("memeContainer");
-	memeContainer.innerHTML = `<img src="${memes[index].url}" alt="Meme">`;
+    const memeContainer = document.getElementById("memeContainer");
+    memeContainer.innerHTML = `<img src="${memes[index].url}" id="current-meme" alt="Meme">`;
 }
 
 // Advice API ------------------------------------------------------------- //
@@ -37,16 +37,17 @@ $("#generate-text").on("click", setQuote);
 // Calls this future to reset or call text under image
 function setQuote() {
 fetch('https://api.adviceslip.com/advice')
-	.then(function(response) {
-		return response.json();
-	})
-	.then(function(data) {
-		var quote = data.slip.advice;
-		console.log(quote);
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        var quote = data.slip.advice;
+        console.log(quote);
 
-		$("#quote-box-text").text(quote);
-	})
+        $("#quote-box-text").text(quote);
+    })
 };
+
 // MODAL  ----------------------------------------------------------------- //
 // var modal = document.querySelector('.modal');
 // var closeButtons = document.querySelectorAll('.close-modal');
@@ -56,7 +57,7 @@ fetch('https://api.adviceslip.com/advice')
 // for (i = 0; i < closeButtons.length; ++i) {
 //   closeButtons[i].addEventListener('click', function() {
 //     modal.classList.toggle('modal-open');
-// 	});
+//  });
 // }
 // // close modal if clicked outside content area
 // document.querySelector('.modal-inner').addEventListener('click', function() {
@@ -64,11 +65,32 @@ fetch('https://api.adviceslip.com/advice')
 // });
 // // prevent modal inner from closing parent when clicked
 // document.querySelector('.modal-content').addEventListener('click', function(e) {
-// 	e.stopPropagation();
+//  e.stopPropagation();
 // });
 // // Allows escape and space key to close for accessiblity purposes. 
 // document.addEventListener("keydown", function(event) {
-// 	if (event.key === "Escape" || event.key === " ") {
-// 		modal.classList.toggle('modal-open');
-// 	}
+//  if (event.key === "Escape" || event.key === " ") {
+//      modal.classList.toggle('modal-open');
+//  }
 // }); 
+
+
+$(document).ready(function() {
+    $("#save-data").click(function() {
+      var quoteText = $("#quote-box-text").text();
+      var memeImage = $("#current-meme").attr("src");
+      console.log(memeImage);
+      localStorage.setItem("quoteText", quoteText);
+      localStorage.setItem("memeImage", memeImage);
+    });
+
+    if (localStorage.getItem("quoteText")) {
+      $("#quote-box-text").text(localStorage.getItem("quoteText"));
+    }
+    if (localStorage.getItem("memeImage")) {
+      $("#current-meme").attr("src", localStorage.getItem("memeImage"));
+    }
+  });
+  
+  
+
